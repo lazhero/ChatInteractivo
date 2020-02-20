a0090fc0ca65b3146ec15fb565160911ee57d1cd
lazhero <54720338+lazhero@users.noreply.github.com>
2020-02-20 06:26:58 -0600

    #3 Separar las comunicaciones de receptor y emisor

--- src/Conexiones/EnlaceServidores.java
+++ src/Conexiones/EnlaceServidores.java
@@ -1,45 +1,39 @@
 package Conexiones;
-
 import java.io.IOException;
 import java.net.ServerSocket;
-import java.net.Socket;
-
-public class Enlace {
+public class EnlaceServidores {
     private  int port;
     private  String ip;
     private boolean ConectadoS=false;
-    Enlace(int port, String ip){
+    EnlaceServidores(int port, String ip){
         this.port=port;
         this.ip=ip;
     }
-    Enlace(){
-      this.port=4000;
-      this.ip="127.0.0.1";
+    EnlaceServidores(){
+        this.port=4000;
+        this.ip="127.0.0.1";
     }
-    Enlace(String ip){
+    EnlaceServidores(String ip){
         this.ip=ip;
         this.port=4000;
     }
-
-    public Socket ConectarEnviarFijo(){
+    public ServerSocket ConectarRecepFijo(){
         try{
-            Socket Receptor=new Socket(this.ip,this.port);
+            ServerSocket Receptor=new ServerSocket(this.port);
             System.out.println("Conexion Exitosa");
             return Receptor;
-
-
         }
         catch(IOException excep){
             System.out.print("El puerto no esta disponible");
-            return this.ConectarEnviarVariable();
+            return this.ConectarRecepVariable();
         }
     }
-    public Socket ConectarEnviarVariable(){
+    public ServerSocket ConectarRecepVariable(){
         this.port=4000;
-        Socket Receptor=null;
+        ServerSocket Receptor=null;
         while(this.port<4100) {
             try {
-                Receptor = new Socket(this.ip,this.port);
+                Receptor = new ServerSocket(this.port);
                 this.ConectadoS=true;
             } catch (IOException excep) {
                 this.port++;
