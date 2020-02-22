fa26d11121e23fcae0f01c86940429553372ef31
lazhero <54720338+lazhero@users.noreply.github.com>
2020-02-22 09:21:27 -0600

    #5 Guardado

--- src/Graficos/InterfazG.java
+++ src/Graficos/InterfazG.java
@@ -2,13 +2,19 @@ package Graficos;
 
 import javax.swing.*;
 
-public class InterfazG {
+
+final public class InterfazG{
     private JFrame MainWdw;
     public InterfazG(String texto,int width,int height){
         MainWdw= new JFrame(texto);
         MainWdw.setSize(width,height);
         MainWdw.setLayout(null);//using no layout managers
         MainWdw.setVisible(true);
+        JPanel Panel=new JPanel();
+        MainWdw.add(Panel) ;
+
+
     }
     
+
 }
--- src/Prueba3.java
+++ src/Prueba3.java
@@ -2,7 +2,7 @@ import Graficos.InterfazG;
 
 public class Prueba3 {
     public static void main(String[] args){
-        InterfazG nuevo =new InterfazG("Ventana");
+        InterfazG nuevo =new InterfazG("Ventana",400,400);
 
     }
 }
