39a67cbded8755ded5c3c3fe22b3cfa87f564098
lazhero <54720338+lazhero@users.noreply.github.com>
2020-02-22 18:49:12 -0600

    #5 Pruebas

---
+++ src/Graficos/Controller.java
@@ -0,0 +1,4 @@
+package Graficos;
+
+public class Controller {
+}
--- src/Graficos/VentanaPrincipal.java
+++ src/Graficos/VentanaPrincipal.java
@@ -7,11 +7,18 @@ import javafx.scene.Scene;
 import javafx.scene.text.Text;
 import javafx.scene.layout.StackPane;
 import javafx.scene.text.Text;
+import java.io.FileInputStream;
 
 
 
 
-public class Graficos extends Application {
+public class VentanaPrincipal extends Application {
+    private  Stage stage;
+    private  Scene scene;
+    private  StackPane stack;
+
+    public VentanaPrincipal() {
+    }
 
     public static void main(String[] args) {
         launch(args);
@@ -19,16 +26,15 @@ public class Graficos extends Application {
 
     @Override
     public void start(Stage primaryStage) {
-        ///primaryStage.setMaxWidth(100.0);
-        ///primaryStage.setMaxHeight(500.0);
-        ///primaryStage.setResizable(false);
-        Text text = new Text();
-        text.setText("Hello !! Welcome to JavaTPoint");
-        StackPane root = new StackPane();
-        Scene scene = new Scene(root,1000,400);
-        root.getChildren().add(text);
+        this.stage=primaryStage;
+        this.stack=new StackPane();
+        this.scene=new Scene(this.stack,1000,400);
+        StackPane root = this.stack;
+        Scene scene = this.scene;
+        //root.getChildren().add(text);
         primaryStage.setScene(scene);
         primaryStage.setTitle("Text Example");
+       // primaryStage.
         primaryStage.show();
 
 
