2a2ff980d151fc156e16be26be46b1b2637f9b6a
lazhero <54720338+lazhero@users.noreply.github.com>
2020-02-22 19:41:18 -0600

    #5 Reparacion de errores

--- src/pruebas.java
+++ src/pruebas.java
@@ -4,6 +4,6 @@ public class pruebas {
     public static void main(String[] args){
         Enlace Cliente=new Enlace(4000, "127.0.0.1");
         Cliente.ConectarEnviarVariable();
-        Cliente.EnviarMensaje("Hola mundo");
+        Cliente.EnviarMensaje("Milton puto amo");
     }
 }
