63ccf76d29a342fdc64035891532df6a3dcbeb6e
lazhero <54720338+lazhero@users.noreply.github.com>
2020-02-22 19:05:50 -0600

    Revert "#5 Pruebas"
    This reverts commit 39a67cbded8755ded5c3c3fe22b3cfa87f564098.

--- src/Graficos/Controller.java
+++
@@ -1,4 +0,0 @@
-package Graficos;
-
-public class Controller {
-}
--- src/Graficos/Graficos.java
+++ src/Graficos/Graficos.java
@@ -7,18 +7,11 @@ import javafx.scene.Scene;
 import javafx.scene.text.Text;
 import javafx.scene.layout.StackPane;
 import javafx.scene.text.Text;
-import java.io.FileInputStream;
 
 
 
 
-public class VentanaPrincipal extends Application {
-    private  Stage stage;
-    private  Scene scene;
-    private  StackPane stack;
-
-    public VentanaPrincipal() {
-    }
+public class Graficos extends Application {
 
     public static void main(String[] args) {
         launch(args);
@@ -26,15 +19,16 @@ public class VentanaPrincipal extends Application {
 
     @Override
     public void start(Stage primaryStage) {
-        this.stage=primaryStage;
-        this.stack=new StackPane();
-        this.scene=new Scene(this.stack,1000,400);
-        StackPane root = this.stack;
-        Scene scene = this.scene;
-        //root.getChildren().add(text);
+        ///primaryStage.setMaxWidth(100.0);
+        ///primaryStage.setMaxHeight(500.0);
+        ///primaryStage.setResizable(false);
+        Text text = new Text();
+        text.setText("Hello !! Welcome to JavaTPoint");
+        StackPane root = new StackPane();
+        Scene scene = new Scene(root,1000,400);
+        root.getChildren().add(text);
         primaryStage.setScene(scene);
         primaryStage.setTitle("Text Example");
-       // primaryStage.
         primaryStage.show();
 
 
