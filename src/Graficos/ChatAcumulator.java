ae70073632d06331566d55ddb1403febc2dead49
lazhero <54720338+lazhero@users.noreply.github.com>
2020-02-24 12:36:35 -0600

    #5 Error encontrado
    Se localiza el error, se elimina la linea, correspondiente

--- src/Graficos/ChatAcumulator.java
+++ src/Graficos/ChatAcumulator.java
@@ -9,6 +9,7 @@ import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
 import javafx.stage.Stage;
 import javafx.event.Event;
 import javafx.scene.Scene;
@@ -18,7 +19,7 @@ import javafx.scene.text.Text;
 import javafx.scene.shape.Rectangle;
 public class ChatAcumulator {
     private static  ArrayList<AnchorPane> Chats=new ArrayList<AnchorPane>();
-    private static Button[] ArrayButton={new Button(),new Button(),new Button(),new Button(),new Button(),new Button(),new Button()};
+    private static Button[] ArrayButton={new Button("chat 1"),new Button("Chat 2"),new Button("Chat 3"),new Button("Chat 4"),new Button("Chat 5"),new Button("Chat 6"),new Button("Chat 7")};
     private int id;
     private static ArrayList<String> HistorialChats;
     private static int NumeroChat=0;
@@ -31,24 +32,29 @@ public class ChatAcumulator {
         }
         NumeroChat++;
         id=NumeroChat;//Potencial Error
-       Button btn=new Button("Chat");
-       btn.setOnAction(e -> ButtonAction());
-       ChatAcumulator.anchor.getChildren().addAll(btn);
+       String Texto ="Chat "+ Integer.toString(id);
+       Button btnk=new Button(Texto);
+      btnk.setOnAction(e->this.ButtonAction());
+
        AnchorPane panel=LayoutCreation.Anchor(400.0,10000.0);
        System.out.println("Hecho");
        Chats.add(panel);
-       double TOP=10.0+20.0*this.id;
+       double TOP=10.0+60*this.id;
        System.out.println(id);
-       LayoutNewContent.Add(ChatAcumulator.anchor,btn,TOP,0.0,0.0,25.0);
+       btnk.setOnAction(e -> ButtonAction());
+       btnk.setShape(new Circle(40));
+       btnk.setStyle("-fx-background-color: #44ff4e; ");
+       LayoutNewContent.Add(ChatAcumulator.anchor,btnk,TOP,0.0,0.0,25.0);
        System.out.println("Hecho");
     }
     public static void Creator(ScrollPane scroll,AnchorPane anchor){
        if(ChatAcumulator.NumeroChat<7){
-           new ChatAcumulator(scroll,anchor);
+          ChatAcumulator Btn= new ChatAcumulator(scroll,anchor);
        }
     }
-    public void ButtonAction(){
-       ChatAcumulator.scroll.setContent(ChatAcumulator.Chats.get(this.id-1));
+    private void ButtonAction(){
+           ChatAcumulator.scroll.setContent(ChatAcumulator.Chats.get(this.id - 1));
+
 
     }
 
--- src/Graficos/VentanaPrincipal.java
+++ src/Graficos/VentanaPrincipal.java
@@ -33,17 +33,14 @@ public class VentanaPrincipal extends Application {
 
     @Override
     public void start(Stage primaryStage) {
-       AnchorPane FirstLevelAnchorPanel=LayoutCreation.Anchor(800.0,600.0);
+      final AnchorPane FirstLevelAnchorPanel=LayoutCreation.Anchor(800.0,600.0);
        Rectangle rectSecondLevel=WidgetCreation.CreateRec(10.0,10.0,300.0,550.0);
-       ScrollPane SecondLevelScrollPane=LayoutCreation.scroll(400.0,550.0);
+       final ScrollPane SecondLevelScrollPane=LayoutCreation.scroll(400.0,550.0);
        Button btn=WidgetCreation.CreateButton("+");
-        btn.setOnAction(new EventHandler() {
-            @Override
-            public void handle(Event event) {
-                ChatAcumulator.Creator(SecondLevelScrollPane,FirstLevelAnchorPanel);
-            }
-
-        });
+       boolean confirmacion=false;
+        btn.setOnAction(e->
+                ChatAcumulator.Creator(SecondLevelScrollPane,FirstLevelAnchorPanel)
+        );
        //AnchorPane ThridLevelAnchorPanel=LayoutCreation.Anchor(400.0,10000.0);
        LayoutNewContent.Add(FirstLevelAnchorPanel,SecondLevelScrollPane,20.0,0.0,0.0,350.0);
        LayoutNewContent.Add(FirstLevelAnchorPanel,btn,0.0,10.0,20.0,0.0);
