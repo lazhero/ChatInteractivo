dd75cd80311a974b10fb1f4271f755554b8dbc49
lazhero <54720338+lazhero@users.noreply.github.com>
2020-02-28 11:24:57 -0600

    #3 Guardado sin posibilidad de prueba
    Se  guarda la posible clase funcion de envio

--- src/Conexiones/ControlEnvio.java
+++ src/Conexiones/ControlEnvio.java
@@ -3,18 +3,22 @@ package Conexiones;
 import Graficos.ChatAcumulator;
 
 public class ControlEnvio {
-    public static void Enviar(){
+    public static void Enviar(String Mensaje){
         int Posicion= ChatAcumulator.getChatActual()-1;
+        Enlace Enviar;
         try{
             int Puerto=ChatAcumulator.getPuerto(Posicion);
-            Enlace Enviar=new Enlace(Puerto);
+            Enviar=new Enlace(Puerto);
             Enviar.ConectarEnviarFijo();
         }
         catch (Exception e){
-            Enlace Enviar=new Enlace();
+            Enviar=new Enlace();
             Enviar.ConectarEnviarVariable();
-            
+            ChatAcumulator.AddPuerto(Enviar.getPort());
         }
+        String MensajeFinal=Integer.toString(HiloServer.getPort(Posicion))+"~"+Mensaje;
+        Enviar.EnviarMensaje(MensajeFinal);
+
 
     }
 
